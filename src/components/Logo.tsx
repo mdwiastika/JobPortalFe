2a280bcd7102bb2ef270f82a419458347abfeb65
Marcel Dwi Astika <marceldwias@gmail.com>
2024-11-13 19:27:30 +0700

    fix: logo flex

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 import logo from "/job-wise2.png";
 export default function Logo() {
   return (
-    <div>
+    <div className="flex justify-start items-center">
       <img src={logo} className={`w-[3em] md:w-[3em] h-auto`} alt="" />
       <span className="font-medium text-sm md:text-sm tracking-widest font-montserrat">
         JobWise
